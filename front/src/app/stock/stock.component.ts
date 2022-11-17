ff4e465ec19622c19170142b09e9d88da4613d7c
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-11-17 17:05:30 +0100

    dev: hidden on condition

--- front/src/app/stock/stock.component.ts
+++ front/src/app/stock/stock.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Article } from '../interfaces/article';
 import { ArticleService } from '../services/article.service';
 
 @Component({
@@ -7,7 +8,16 @@ import { ArticleService } from '../services/article.service';
   styleUrls: ['./stock.component.scss'],
 })
 export class StockComponent implements OnInit {
+  selectedArticles = new Set<Article>();
   constructor(protected articleService: ArticleService) {}
 
   ngOnInit(): void {}
+
+  toggle(a: Article) {
+    if (this.selectedArticles.has(a)) {
+      this.selectedArticles.delete(a);
+      return;
+    }
+    this.selectedArticles.add(a);
+  }
 }
