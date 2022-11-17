917fa93ede6525bd30690263a2a59b408a8a49e8
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-11-17 15:21:58 +0100

    dev: make form reactive

--- front/src/app/stock/create/create.component.ts
+++ front/src/app/stock/create/create.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
-  styleUrls: ['./create.component.scss']
+  styleUrls: ['./create.component.scss'],
 })
 export class CreateComponent implements OnInit {
+  f = new FormGroup({
+    name: new FormControl('Truc', [
+      Validators.required,
+      Validators.minLength(3),
+    ]),
+    price: new FormControl(1, [Validators.required]),
+    qty: new FormControl(1, [Validators.required]),
+  });
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- front/src/app/stock/stock.module.ts
+++ front/src/app/stock/stock.module.ts
@@ -1,19 +1,12 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CreateComponent } from './create/create.component';
 import { StockRoutingModule } from './stock-routing.module';
 import { StockComponent } from './stock.component';
-import { CreateComponent } from './create/create.component';
-
 
 @NgModule({
-  declarations: [
-    StockComponent,
-    CreateComponent
-  ],
-  imports: [
-    CommonModule,
-    StockRoutingModule
-  ]
+  declarations: [StockComponent, CreateComponent],
+  imports: [CommonModule, StockRoutingModule, ReactiveFormsModule],
 })
-export class StockModule { }
+export class StockModule {}
