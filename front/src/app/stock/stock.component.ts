892b1cae3d918b6da4899e57fc6e86618dce0f95
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-11-17 14:18:05 +0100

    dev: added loop
    and interface article

--- front/src/app/stock/stock.component.ts
+++ front/src/app/stock/stock.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Article } from '../interfaces/article';
 
 @Component({
   selector: 'app-stock',
   templateUrl: './stock.component.html',
-  styleUrls: ['./stock.component.scss']
+  styleUrls: ['./stock.component.scss'],
 })
 export class StockComponent implements OnInit {
+  articles: Article[] = [
+    {
+      name: 'Tournevis',
+      price: 2.5,
+      qty: 123,
+    },
+    {
+      name: 'Marteau',
+      price: 2.25,
+      qty: 25,
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
