0c3dbe1edcc14ac4fbb881d64a29c474d331405c
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-11-18 09:50:43 +0100

    dev: added fontawesome

--- front/src/app/stock/stock.module.ts
+++ front/src/app/stock/stock.module.ts
@@ -1,12 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { CreateComponent } from './create/create.component';
 import { StockRoutingModule } from './stock-routing.module';
 import { StockComponent } from './stock.component';
 
 @NgModule({
   declarations: [StockComponent, CreateComponent],
-  imports: [CommonModule, StockRoutingModule, ReactiveFormsModule],
+  imports: [
+    CommonModule,
+    StockRoutingModule,
+    ReactiveFormsModule,
+    FontAwesomeModule,
+  ],
 })
 export class StockModule {}
